3f1ebd1c97f39a3c81ecc2241e245a7d99040a7a
malasia-415 <55638544+malasia-415@users.noreply.github.com>
2020-03-05 14:03:50 -0800

    Create index.js

---
+++ index.js
@@ -0,0 +1,15 @@
+//import React from 'react';
+//import ReactDOM from 'react-dom';
+//import './style.css';
+//import STORE from './STORE'
+//import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './style.css'
+import App from './App'
+
+ReactDOM.render(
+  //<App store={STORE} />,
+  <App />,
+  document.getElementById('root'),
+);
